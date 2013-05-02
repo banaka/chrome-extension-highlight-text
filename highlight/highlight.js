b1f4a10a85833e07e1f613e9a4f3f04b02297ce8
To Bao Thien Quan <btquanto@gmail.com>
2013-05-02 18:01:27 +0700

    Save highlighted text into local storage

--- highlight/highlight.js
+++ highlight/highlight.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
 	$(document.body).annotator();
+	var content = $(document.body).annotator();
+	content.annotator('addPlugin', 'Offline', {
+		online : function() {
+			jQuery("#status").text("Online");
+		},
+		offline : function() {
+			jQuery("#status").text("Offline");
+		}
+	});
+	var annotator = content.data('annotator');
+
+	jQuery("#clear-storage").click(function() {
+		if (annotator) {
+			annotator.plugins.Offline.store.clear()
+		}
+	});
 });
