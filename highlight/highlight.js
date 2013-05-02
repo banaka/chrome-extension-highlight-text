58b82ca89dcb0abc408116fec4ab8c1ccd495d03
To Bao Thien Quan <btquanto@gmail.com>
2013-05-02 18:14:26 +0700

    Remove redundant codes

--- highlight/highlight.js
+++ highlight/highlight.js
@@ -1,19 +1,6 @@
 $(document).ready(function() {
 	$(document.body).annotator();
-	var content = $(document.body).annotator();
-	content.annotator('addPlugin', 'Offline', {
-		online : function() {
-			jQuery("#status").text("Online");
-		},
-		offline : function() {
-			jQuery("#status").text("Offline");
-		}
-	});
+	var content = jQuery(document.body).annotator();
+	content.annotator('addPlugin', 'Offline');
 	var annotator = content.data('annotator');
-
-	jQuery("#clear-storage").click(function() {
-		if (annotator) {
-			annotator.plugins.Offline.store.clear()
-		}
-	});
 });
